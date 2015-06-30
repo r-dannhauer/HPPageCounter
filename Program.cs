d66fba1617591784aaba63520d6e3759ba93f5ce
Robert Dannhauer <r.dannhauer@posteo.de>
2015-06-30 13:47:28 +0200

    More changes for full release.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,26 @@
 ﻿using System;
-using MySql.Data;
-using MySql.Data.MySqlClient;
-using MySql.Data.Types;
+//using MySql.Data;
+//using MySql.Data.MySqlClient;
+//using MySql.Data.Types;
 using System.Net.NetworkInformation;
 using System.Net;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace HPPageCounter
 {
 	class MainClass
 	{
 		//Classes for the lists
+
 		public class Printer{
 			public string DEP { get; set; }
 			public string SN { get; set; }
 			public string IP { get; set; }
 			public bool Active { get; set; }
-//			public int PrinterWebType { get; set; }
 			public int PageCounterMono { get; set; }
 			public int PageCounterColor { get; set; }
 		}
@@ -28,6 +29,7 @@ namespace HPPageCounter
 			public int PageCounterMono { get; set; }
 			public int PageCounterColor { get; set; }
 			public bool Active { get; set; }
+			public int WebType { get; set; }
 		}
 		public class ExportData{
 			public string DEP { get; set; }
@@ -39,88 +41,86 @@ namespace HPPageCounter
 			public bool Connected { get; set; }
 			public bool NewSN { get; set; }
 		}
+
 		//Classes for the lists END
 
+		[STAThread]
 		public static void Main (string[] args)
 		{
+			
 			//Creating variables
+
 			bool bDebug = true;
-			bool bDBconnect = false;
-			string sDBconnection = "";
+			string sLine;
+			string [] sPrinters;
 			var Printerlist = new List<Printer> ();
 			var NewDatalist = new List<NewInfos> ();
 			var Exportlist = new List <ExportData> ();
 			Ping pConnected = new Ping ();
 			IPAddress ipConnected;
-			MySql.Data.MySqlClient.MySqlConnection sqlConnection = new MySql.Data.MySqlClient.MySqlConnection (sDBconnection);
+			StreamReader sr;
+			StreamWriter sw;
+			StreamWriter logfile;
+			StreamWriter sw2;
+
 			//Creating variables END
 
-			//establish MySQL Connection
-			if (bDBconnect) {
-				System.Console.WriteLine ("Opening DB connection.");
-				try {
-					sqlConnection.Open ();
-				} catch (MySql.Data.MySqlClient.MySqlException ex) {
-					if (bDebug) {
-						System.Console.WriteLine ("Program stopped because of a critical error: Failed connecting to the Database!");
-						System.Console.WriteLine (ex.Message);
-						System.Console.ReadLine ();
-						return;
-					} else {
-						//TODO: Logs schreiben
-						return;
-					}
-				}
-			}else{
-				System.Console.WriteLine ("DB connection disabled.");
-			}
-			//establish MySQL Connection END
 
-			//reading tables
-			if (bDBconnect){
-				System.Console.WriteLine ("Reading Printer list.");
-			}else{
-				System.Console.WriteLine ("No DB connection, creating test printers.");
-				Printerlist.Add (new Printer {
-					DEP = "DEP06743",
-					SN = "DXXXS",
-					IP = "127.0.0.1",
-					Active = true,
-//					PrinterWebType = 2,
-					PageCounterMono = 0,
-					PageCounterColor = 0
-				});
-//				Printerlist.Add (new Printer {
-//					DEP = "DEP06743",
-//					SN = "CXXXS",
-//					IP = "127.0.0.1",
-//					Active = true,
-//					PrinterWebType = 1,
-//					PageCounterMono = 0,
-//					PageCounterColor = 0
-//				});
+			//reading CSV file
+
+			OpenFileDialog openFileDialog1 = new OpenFileDialog ();
+			if (openFileDialog1.ShowDialog () == DialogResult.OK) {
+				sr = new StreamReader (openFileDialog1.FileName);
+				logfile = new StreamWriter (openFileDialog1.FileName + "_logfile.txt");
+			} else {
+				System.Console.WriteLine ("Program stopped because of a critical error: No file chosen!");
+				System.Console.ReadLine ();
+				return;
 			}
-			//reading tables END
+
+			//reading CSV file END
+
+
+			//reading printers
+
+			System.Console.WriteLine ("Reading Printer list.");
+			logfile.WriteLine ("Reading Printer list.");
+			while ((sLine = sr.ReadLine()) != null){
+				sPrinters = sLine.Split (';');
+				Printerlist.Add (new Printer {
+					DEP = sPrinters [0],
+					SN = sPrinters [1],
+					IP = sPrinters [2],
+					Active = Convert.ToBoolean(sPrinters [3]),
+					PageCounterMono = Convert.ToInt32(sPrinters [4]),
+					PageCounterColor = Convert.ToInt32(sPrinters [5])
+				});
+			}
+			logfile.WriteLine ("Read " + Printerlist.Count + " printers.");
+
+			//reading printers END
+
 
 			//Query printers
+
 			for (int i=0; i < Printerlist.Count; i++)
 			{
+				
 				//Check if IP is valid
 				if (IPAddress.TryParse (Printerlist [i].IP, out ipConnected)) {
-					if (bDebug && !bDBconnect) {
+					if (bDebug) {
 						System.Console.WriteLine ("Valid IP Adress.");
 					}
 				}else{
 					if (bDebug){
 						System.Console.WriteLine ("Invalid IP Adress: " + Printerlist [i].DEP + " " + Printerlist [i].IP);
-					}else{
-						//TODO: Log schreiben
 					}
+					logfile.WriteLine("WARNING: Invalid IP Adress: " + Printerlist [i].DEP + " " + Printerlist [i].IP);
 					continue;
 				}
 
 				//Check if printer is online
-				if (bDebug && !bDBconnect) {
+				if (bDebug) {
 					System.Console.WriteLine ("Pinging printer.");
 				}
 				if (pConnected.Send (ipConnected).Status == IPStatus.Success) {
@@ -131,19 +131,21 @@ namespace HPPageCounter
 					if(bDebug){
 						System.Console.WriteLine ("Host unreachable!");
 					}else if(Printerlist[i].Active == true){
-						//TODO: Log schreiben
+						logfile.WriteLine ("WARNING: Active printer not reachable! " + Printerlist [i].DEP + " " + Printerlist [i].IP);
 						NewDatalist.Add (new NewInfos {
 							SN = Printerlist [i].SN,
 							PageCounterMono = Printerlist [i].PageCounterMono,
 							PageCounterColor = Printerlist [i].PageCounterColor,
-							Active = false
+							Active = false,
+							WebType = 0
 						});
 					}else{
 						NewDatalist.Add (new NewInfos {
 							SN = Printerlist [i].SN,
 							PageCounterMono = Printerlist [i].PageCounterMono,
 							PageCounterColor = Printerlist [i].PageCounterColor,
-							Active = false
+							Active = false,
+							WebType = 0
 						});
 					}
 					continue;
@@ -151,7 +153,11 @@ namespace HPPageCounter
 
 				//Quering printer and writing the results in a list
 				NewDatalist.Add (QueryPrinter (Printerlist [i].IP, bDebug));
+				if (NewDatalist [i].WebType == 0) {
+					logfile.WriteLine ("WARNING: Printer " + Printerlist [i].DEP + " - " + Printerlist[i].IP + " has no WebType!");
+				}
 			}
+
 			//Query printers END
 
 			if(bDebug){
@@ -161,6 +167,7 @@ namespace HPPageCounter
 			}
 
 			//Write infos into ExportDatalist and calculate Pagecount difference
+
 			for (int i=0; i < NewDatalist.Count; i++){
 				bool bNewSN=false;
 				if (NewDatalist[i].SN != Printerlist[i].SN) {bNewSN = true;}
@@ -177,6 +184,7 @@ namespace HPPageCounter
 				Printerlist[i].PageCounterMono = NewDatalist[i].PageCounterMono;
 				Printerlist[i].PageCounterColor = NewDatalist[i].PageCounterColor;
 			}
+
 			//Write infos into ExportDatalist and calculate Pagecount difference END
 
 			if (bDebug){
@@ -191,32 +199,76 @@ namespace HPPageCounter
 					+ Exportlist [i].NewSN
 					);
 				}
+			}
+
+			//Exporting into CSV
+
+			SaveFileDialog saveFileDialog1 = new SaveFileDialog ();
+			if (saveFileDialog1.ShowDialog () == DialogResult.OK) {
+				sw = new StreamWriter (saveFileDialog1.FileName);
+			} else {
+				System.Console.WriteLine ("Program stopped because of a critical error: No file chosen!");
 				System.Console.ReadLine ();
+				return;
 			}
-			if (bDBconnect){
-				//TODO: Daten in die DB schreiben
+			for (int i = 0; i < Exportlist.Count; i++) {
+				//Export for new data
+				sLine = (Exportlist [i].DEP + ";"
+					+ Exportlist [i].SN + ";"
+					+ Exportlist [i].PageCounterMono + ";"
+					+ Exportlist [i].PageCounterColor + ";"
+					+ Exportlist [i].MonthlyPagesMono + ";"
+					+ Exportlist [i].MonthlyPagesColor + ";"
+					+ Exportlist [i].Connected + ";"
+					+ Exportlist [i].NewSN
+				);
+				sw.WriteLine (sLine);
+				//Export for new Printerlist
+				sLine = (Exportlist [i].DEP + ";"
+					+ Exportlist [i].SN + ";"
+					+ Printerlist [i].IP + ";"
+					+ Exportlist [i].Connected + ";"
+					+ Exportlist [i].PageCounterMono + ";"
+					+ Exportlist [i].PageCounterColor
+				);
+				sw2.WriteLine (sLine);
 			}
+			sw.Close ();
+			sw2.Close ();
+			logfile.WriteLine ("Exported " + Exportlist.Count + " printers successfully.");
+			Console.WriteLine ("Exported " + Exportlist.Count + " printers successfully.");
 
+			//Exporting into CSV END
 
+			logfile.Close ();
+			Console.ReadLine ();
 		}
+
+
 		public static NewInfos QueryPrinter(string sIP, bool bDebug)
 		{
+			
 			//initialize variables
+
 			string sURL = "127.0.0.1";
 			string sSN = "";
-			string sHTML, sMatchPattern;
+			string sHTML, sMatchPattern, sTemp;
 			int iPageCounterMono = 0;
 			int iPageCounterColor = 0;
 			Regex regEx;
 			Match regMatch;
+
 			//initialize variables END
+
 
 			//accept all certificates
 			ServicePointManager.ServerCertificateValidationCallback = delegate {return true;};
 
 			//create URL string
+
 			switch (getWebType(sIP)){
 			case 1:
+			case 2:
 				sURL = "https://" + sIP + "/hp/device/InternalPages/Index?id=UsagePage";
 				break;
 
@@ -224,21 +276,26 @@ namespace HPPageCounter
 				if(bDebug){
 					System.Console.WriteLine("No WebType for printer " + sIP);
 				}
-				//TODO: Log schreiben
-				return new NewInfos {SN = "NoWebType", PageCounterMono = 0, PageCounterColor = 0, Active = false};
+				return new NewInfos {SN = "NoWebType", PageCounterMono = 0, PageCounterColor = 0, Active = false, WebType = 0};
 			}
+
 			//create URL string END
 
+
 			//read source code from website
+
 			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create (sURL);
 			HttpWebResponse response = (HttpWebResponse)request.GetResponse ();
 			StreamReader sr = new StreamReader (response.GetResponseStream ());
 			sHTML = sr.ReadToEnd ();
 			sr.Close ();
 			response.Close ();
+
 			//read source code from website END
 
+
 			//getting informations with the webtype
+
 			switch (getWebType(sIP)){
 			case 1:
 				sMatchPattern = "id=\"UsagePage[.]DeviceInformation[.]DeviceSerialNumber\">(..........)<";
@@ -246,46 +303,70 @@ namespace HPPageCounter
 				regMatch = regEx.Match (sHTML);
 				sSN = regMatch.Groups [1].Value;
 
-				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Print[.]Monochrome\" class=\"align-right\">(\\d+).\\d+<";
+				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Print[.]Monochrome\" class=\"align-right\">(\\d+)[.]\\d<";
 				regEx = new Regex (sMatchPattern);
 				regMatch = regEx.Match (sHTML);
+
 				iPageCounterMono = Convert.ToInt32 (regMatch.Groups [1].Value);
 
-				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Print[.]Color\" class=\"align-right\">(\\d+).\\d+<";
+				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Print[.]Color\" class=\"align-right\">(\\d+)[.]\\d<";
 				regEx = new Regex (sMatchPattern);
 				regMatch = regEx.Match (sHTML);
-				iPageCounterColor = Convert.ToInt32 (regMatch.Groups [1].Value);
+				sTemp = regMatch.Groups[1].Value.Replace(",", "");
+				iPageCounterColor = Convert.ToInt32 (sTemp);
 
-				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Copy[.]Monochrome\" class=\"align-right\">(\\d+).\\d+<";
+				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Copy[.]Monochrome\" class=\"align-right\">(\\d+)[.]\\d<";
 				regEx = new Regex (sMatchPattern);
 				regMatch = regEx.Match (sHTML);
-				iPageCounterMono += Convert.ToInt32 (regMatch.Groups [1].Value);
+				sTemp = regMatch.Groups[1].Value.Replace(",", "");
+				iPageCounterMono += Convert.ToInt32 (sTemp);
 
-				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Copy[.]Color\" class=\"align-right\">(\\d+).\\d+<";
+				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Copy[.]Color\" class=\"align-right\">(\\d+)[.]\\d<";
 				regEx = new Regex (sMatchPattern);
 				regMatch = regEx.Match (sHTML);
-				iPageCounterColor += Convert.ToInt32 (regMatch.Groups [1].Value);
+				sTemp = regMatch.Groups[1].Value.Replace(",", "");
+				iPageCounterColor += Convert.ToInt32 (sTemp);
 
-				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Fax[.]Monochrome\" class=\"align-right\">(\\d+).\\d+<";
+				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Fax[.]Monochrome\" class=\"align-right\">(\\d+)[.]\\d<";
 				regEx = new Regex (sMatchPattern);
 				regMatch = regEx.Match (sHTML);
-				iPageCounterMono += Convert.ToInt32 (regMatch.Groups [1].Value);
+				sTemp = regMatch.Groups[1].Value.Replace(",", "");
+				iPageCounterMono += Convert.ToInt32 (sTemp);
 
-				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Fax[.]Color\" class=\"align-right\">(\\d+).\\d+<";
+				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Fax[.]Color\" class=\"align-right\">(\\d+)[.]\\d<";
 				regEx = new Regex (sMatchPattern);
 				regMatch = regEx.Match (sHTML);
-				iPageCounterColor += Convert.ToInt32 (regMatch.Groups [1].Value);
+				sTemp = regMatch.Groups[1].Value.Replace(",", "");
+				iPageCounterColor += Convert.ToInt32 (sTemp);
+
+				break;
+
+			case 2:
+				sMatchPattern = "id=\"UsagePage[.]DeviceInformation[.]DeviceSerialNumber\">(..........)<";
+				regEx = new Regex (sMatchPattern);
+				regMatch = regEx.Match (sHTML);
+				sSN = regMatch.Groups [1].Value;
+
+				sMatchPattern = "id=\"UsagePage[.]EquivalentImpressionsTable[.]Print[.]Total\" class=\"align-right\">(\\d+)[.]\\d<";
+				regEx = new Regex (sMatchPattern);
+				regMatch = regEx.Match (sHTML);
+				sTemp = regMatch.Groups [1].Value.Replace (",", "");
+				iPageCounterMono = Convert.ToInt32 (sTemp);
 
 				break;
 
 			}
-			return new NewInfos {SN = sSN, PageCounterMono = iPageCounterMono, PageCounterColor = iPageCounterColor, Active = true};
+			return new NewInfos {SN = sSN, PageCounterMono = iPageCounterMono, PageCounterColor = iPageCounterColor, Active = true, WebType = getWebType(sIP)};
+
 			//getting informations with the webtype END
+
 		}
 
 		public static int getWebType(string sIP)
 		{
+
 			//determines the WebType by checking the modelname
+
 			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create ("https://" + sIP);
 			HttpWebResponse response = (HttpWebResponse)request.GetResponse ();
 			StreamReader sr = new StreamReader (response.GetResponseStream ());
@@ -296,9 +377,16 @@ namespace HPPageCounter
 			Regex regEx = new Regex (sMatchPattern);
 			Match regMatch = regEx.Match (sHTML);
 			switch (regMatch.Groups[1].Value){
+
 			//Cases for Webtype 1
+
 			case "HP Officejet Color FlowMFP X585":
 				return 1;
+
+			//Cases for Webtype 2
+
+			case "HP LaserJet 600 M602":
+				return 2;
 
 			default:
 				return 0;
--- ReadMe.txt
+++ ReadMe.txt
@@ -11,13 +11,13 @@ Index:
 
 1. Purpose of The Software
 ==========================
-This tool queries a database to read the "printer" table and queries the printers in said table to get the following information:
+This tool reads a CSV file and queries the printers in said file to get the following information:
 	- Hostname (DEP)
 	- Serialnumber
 	- Pagecounter for monochromatic prints
 	- Pagecounter for colored prints
 	- bool if the printer is currently connected
-It then queries the printer and reads the pagecounters from the HP usage page, calculates differences and writes these infos in a database and exports the contents to a .csv file.
+It then queries the printer and reads the pagecounters from the HP usage page, calculates differences and writes these infos in a .csv file.
 
 This program works in the command line of Microsoft Windows and is coded in C#.
 
@@ -26,7 +26,6 @@ This program works in the command line of Microsoft Windows and is coded in C#.
 ===============
 The obvious ones:
 	- HP printers
-	- a MySQL server
 	- .Net 4.5 (recommended. older versions should work)
 
 The "not-so-obvious" ones:
@@ -43,8 +42,5 @@ Right now? Uhm... Just add printers as a list element. This is somekind of an "E
 Note: Other printers MIGHT work with this. This list has been tested by me or others. Want a specific model? Open an Issue with the model and send me the HTML code via gist. Or code it and add it, maybe even share it with me. That'd be nice.
 	- Office Jet
 		- Color FlowMFP X585
-
-		
-5. Changelog
-============
-2015-07-XX: Release. What? Every feature is explained in this document. Don't expect me to write everything down here, please! It's a changelog, not a "release-feature-detail-list".
+	- LaserJet
+		- 600 M602
